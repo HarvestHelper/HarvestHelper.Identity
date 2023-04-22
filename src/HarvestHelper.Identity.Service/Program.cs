bb81414191bdabd887e60a23204c226624e396b0
DaurteSomsen <somsenduarte@gmail.com>
2023-04-22 21:40:56 +0200

    Added support for Azure Key Vault

--- src/HarvestHelper.Identity.Service/Program.cs
+++ src/HarvestHelper.Identity.Service/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HarvestHelper.Identity.Service;
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HarvestHelper.Identity.Service
 {
@@ -19,6 +15,15 @@ namespace HarvestHelper.Identity.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, configurationBuilder) => 
+                {
+                    if (context.HostingEnvironment.IsProduction()){
+                        configurationBuilder.AddAzureKeyVault(
+                            new Uri("https://harvesthelper.vault.azure.net/"),
+                            new DefaultAzureCredential()
+                        );
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
