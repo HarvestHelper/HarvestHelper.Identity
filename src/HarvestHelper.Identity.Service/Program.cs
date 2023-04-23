e3e69cc8e5de5734d213df88bc42455b9c088373
DaurteSomsen <somsenduarte@gmail.com>
2023-04-23 18:07:26 +0200

    Use azure keyvault secrets

--- src/HarvestHelper.Identity.Service/Program.cs
+++ src/HarvestHelper.Identity.Service/Program.cs
@@ -1,7 +1,5 @@
-using System;
-using Azure.Identity;
+using HarvestHelper.Common.Configuration;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace HarvestHelper.Identity.Service
@@ -15,15 +13,7 @@ namespace HarvestHelper.Identity.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, configurationBuilder) => 
-                {
-                    if (context.HostingEnvironment.IsProduction()){
-                        configurationBuilder.AddAzureKeyVault(
-                            new Uri("https://harvesthelper.vault.azure.net/"),
-                            new DefaultAzureCredential()
-                        );
-                    }
-                })
+                .ConfigureAzureKeyVault()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
