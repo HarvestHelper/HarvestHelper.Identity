acf46a5423e5d087c33c01443b7ce88c2c1cfcac
Duarte Somsen <ds@letsgrow.com>
2023-05-23 23:23:32 +0200

    Updated tests

--- src/HarvestHelper.Identity.Tests/UnitTest1.cs
+++ src/HarvestHelper.Identity.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
+using HarvestHelper.Identity.Service.Controllers;
+using HarvestHelper.Identity.Service.Dtos;
+using HarvestHelper.Identity.Service.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
 namespace HarvestHelper.Identity.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
-
-    }
+    
 }
